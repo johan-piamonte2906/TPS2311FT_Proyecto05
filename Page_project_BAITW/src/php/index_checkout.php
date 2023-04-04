1f73b983c8b8e91b0cc1d088c70143086a43061d
johan-piamonte2906 <jhpiamonte16@misena.edu.co>
2023-04-03 22:38:47 -0700

    Ver carrito Compras

--- Page_project_BAITW/src/php/index_checkout.php
+++ Page_project_BAITW/src/php/index_checkout.php
@@ -2,66 +2,24 @@
 
   require './Conexiones/config.php';
   require './Conexiones/database.php';
+
   $db = new Database();
   $con = $db->conectar();
 
-    $id = isset($_GET['id']) ? $_GET['id']  : '';
-    $token = isset($_GET['token']) ? $_GET['token']  : '';
+  $productos = isset($_SESSION['carrito']['productos']) ? $_SESSION['carrito']['productos'] : null;
 
-    if($id == ''  ||  $token == ''){
-        echo '
-          <script>
-            alert("Error al precesar");
-            window.location.href = "./index_bycle.php";
-          </script>
-        ';
-        exit;
-    } else {
-        
-      $token_tmp = hash_hmac('sha1', $id, KEY_TOKEN);
-        if($token == $token_tmp){
-          $sql = $con->Prepare("SELECT count(id) FROM products WHERE id=? AND activo=1");
-          $sql->execute([$id]);
-            if($sql->fetchColumn() > 0){ 
-              $sql = $con->Prepare("SELECT nombres, descrip, precio, descuento FROM products WHERE id=? AND activo=1 LIMIT 1");
-              $sql->execute([$id]);
-              $row = $sql->fetch(PDO::FETCH_ASSOC);
-              $nombres = $row['nombres'];
-              $descrip = $row['descrip'];
-              $precio = $row['precio'];
-              $descuento = $row['descuento'];
-              $precio_desc = $precio - (($precio * $descuento) / 100);
-              $dir_images = '../img/productos/'. $id .'/';
-                
-              $rutaImg = $dir_images . 'index.jpg';
+  $lista_carrito = array();
 
-                if(!file_exists($rutaImg)){
-                    $rutaImg = '../img/no-photo.jpg';
-                }
-
-                $imagenes = array();
-                  if(file_exists($dir_images)){
-                      $dir = dir($dir_images);
-        
-                      while(($archivo = $dir->read()) != false){
-                        if($archivo != 'index.jpg' && (strpos($archivo,'jpg') || strpos($archivo, 'jpeg'))){
-                          $imagenes[] = $dir_images . $archivo;
-                        }
-                      }
-                      $dir->close();
-                    }
-            }
-            
-        } else {
-          echo '
-            <script>
-              alert("Error al precesar");
-              window.location.href = "./index_bycle.php";
-            </script>
-          ';
-          exit;
+    if($productos != null){
+        foreach($productos as $clave => $cantidad){
+            $sql = $con->Prepare("SELECT id, nombres, precio, descuento, $cantidad AS cantidad FROM products WHERE id=? AND activo=1");
+            $sql->execute([$clave]);
+            $lista_carrito[] = $sql->fetch(PDO::FETCH_ASSOC);
         }
-    }  
+    }
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -70,7 +28,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>B A I A T W  | inicio </title>
+    <title>B A I A T W  | Bikes </title>
     <!--  Styles  -->
     <link rel="stylesheet" href="../css/style_index.css">
     <link rel="stylesheet" href="../css/style_products.css">
@@ -103,31 +61,30 @@
           <i class="bi bi-bicycle "class="d-inline-block align-text-top"></i>
           <span>B A I A T W</span>
         </a>
-
-          <!--  Button  -->
+            <!--  Button  -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="border: 1px solid black;">
-          <!-- <span class="navbar-toggler-icon bg-secondary"></span> -->
-          <i class="navbar-toggle-icon bi bi-list"></i>
+              <!-- <span class="navbar-toggler-icon bg-secondary"></span> -->
+              <i class="navbar-toggle-icon bi bi-list"></i>
         </button>
 
         <div class="collapse navbar-collapse" id="menu">
-          <ul class="navbar-nav">
-            <li class="nav-item dropdown">
-              <nav class="navbar d-md-none sticky-md">
-                <div class="container-fluid">
-                  <form class="d-flex">
-                      <input class="form-control me-2 d-flex p-2" type="search" placeholder="Search" aria-label="Search">
-                      <button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
-                  </form>
-                </div>
-              </nav>
-            </li>
-            <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Events</a></li>
-            <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Maintenance</a></li>
-            <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Accessories</a></li>
-            <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Replace</a></li>
-            <li class="nav-item"><a href="./index_bycle.php" class="me-auto nav-link active color-dark">Bikes</a></li>
-          </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <nav class="navbar d-md-none sticky-md">
+                            <div class="container-fluid">
+                                <form class="d-flex">
+                                    <input class="form-control me-2 d-flex p-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
+                                </form>
+                            </div>
+                        </nav>
+                    </li>
+                    <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Events</a></li>
+                    <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Maintenance</a></li>
+                    <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Accessories</a></li>
+                    <li class="nav-item"><a href="#" class="me-auto nav-link active color-dark">Replace</a></li>
+                    <li class="nav-item"><a href="./index_bycle.php" class="me-auto nav-link active color-dark">Bikes</a></li>
+                </ul>
         </div>
 
         <nav class="navbar d-none d-md-block">
@@ -148,62 +105,64 @@
           </div>
         </nav>
       </div>
-    </nav>    
+    </nav>
   <!-- /Nav -->
 
   <!--  Cont page  -->
     <main>
-      <div class="container-fluid pt-3">
-        <a href="./index_bycle.php" style="color: black; text-decoration:none;">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-caret-left-fill" viewBox="0 0 16 16">
-            <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
-          </svg>
-          Regresar
-        </a>
-      </div>
       <div class="container pt-5 pb-5">
-      <div class="row">
-        <div class="col-md-6 order-md-1">
-          <div id="carouselImages" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="<?php echo $rutaImg; ?>" class="d-block w-100">
-                  </div>
-                  <?php foreach($imagenes as $img) { ?>
-                    <div class="carousel-item">
-                      <img src="<?php echo $img; ?>" class="d-block w-100">
-                    </div>
-                  <?php } ?>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselImages" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselImages" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-              </button>
-            </div>
-          </div>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Subtotal</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if($lista_carrito == null){
+                        echo '<tr><td colspan="5" class="text-center"><b>Lista Vacia</b></td></tr>';
+                    }else {
+                        $total = 0;
+                        foreach($lista_carrito as $producto){
+                            $_id = $producto['id'];
+                            $nombre = $producto['nombres'];
+                            $precio = $producto['precio'];
+                            $descuento = $producto['descuento'];
+                            $cantidad = $producto['cantidad'];
+                            $precio_desc = $precio - (($precio * $descuento) / 100);
+                            $subtotal = $cantidad * $precio_desc;
+                            $total += $subtotal;
+                        ?>
+                        <tr>
+                            <td><?php echo $nombre?></td>
+                            <td><?php echo MONEDA . number_format($precio_desc, 3, '.', ','); ?></td>
+                            <td>
+                                <input type="number" min="1" max="5" step="1" value="<?php echo $cantidad; ?>" size="5" id="cantidad_<?php echo $_id; ?>" onchange="actualizarCantidad(this.value, <?php echo $_id; ?>)">
+                            </td>
+                            <td>
+                                <div id="subtotal_<?php echo $_id; ?>" name="subtotal[]"><?php echo MONEDA . number_format($subtotal, 3, '.', ','); ?></div>
+                            </td>
+                            <td><a href="#" id="eliminar" class="btn btn-warning btn-sm" data-bs-id="<?php echo $_id; ?>" data-bs-toggle="modal" data-bs-target="eliminaModal"><i class="fa-solid fa-trash"></i></a></td>
+                        </tr>
+                        <?php }?>
+                        <tr>
+                            <td colspan="3"></td>
+                            <td colspan="2">
+                                <p class="h3" id="total"><?php echo MONEDA . number_format($total, 3, '.', ','); ?></p>
+                            </td>
+                        </tr>
+                </tbody>
+                <?php } ?>
+            </table>
         </div>
-        <div class="col-md-6 order-md-2">
-          <h2><?php echo $nombres; ?></h2>
-
-          <?php if($descuento > 0)  { ?>
-              <p><del><?php echo MONEDA . number_format($precio, 3, '.', ','); ?></del></p>
-              <h2>
-                  <?php echo MONEDA . number_format($precio_desc, 3, '.', ','); ?>
-                  <small class="text-success"><?php echo $descuento; ?>% descuento</small>
-              </h2>
-          <?php }else { ?>
-
-              <h2><?php echo MONEDA . number_format($precio, 3, '.', ','); ?></h2>
-
-          <?php } ?>
-          <p class="lead"><?php echo $descrip; ?></p>
-          <div class="d-grid gap-3 col-10 mx-auto pb-4 pt-3">
-            <button class="btn btn-primary" type="button">Comprar ahora</button>
-            <button class="btn btn-outline-primary" onclick="addProducto(<?php echo $id; ?>, '<?php echo $token_tmp; ?>' )">Agregar al carrito</button>
-          </div>
+        <div class="row">
+            <div class="col-md-5 offset-md-7 d-grid gap-2">
+                <button class="btn btn-primary btn-lg">Realizar pago</button>
+            </div>
         </div>
       </div>
     </main>
@@ -276,9 +235,9 @@
     </footer>
   <!-- /Footer -->
 
-  <!-- Javascript -->
-    <script src="../js/app-carritoCompras.js"></script>
+  <!-- javascript -->
     <script src="../js/app-loader.js"></script>
+    <script src="../js/app-carritoCompras.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
