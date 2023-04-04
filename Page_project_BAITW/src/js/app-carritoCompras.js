0c24ec9c758b34f31be5d2080bcb072744f10e05
johan-piamonte2906 <jhpiamonte16@misena.edu.co>
2023-04-03 21:21:18 -0700

    carrito de compras funcional

--- Page_project_BAITW/src/js/app-carritoCompras.js
+++ Page_project_BAITW/src/js/app-carritoCompras.js
@@ -1,18 +1,18 @@
 function addProducto(id, token){
-    let url = '../php/carrito-compras/index-carrito.php'
-    let formData = new FormData()
-    formData.append('id', id)
-    formData.append('token', token)
+    let url = '../php/carrito-compras/index-carrito.php';
+    let formData = new FormData();
+    formData.append('id', id);
+    formData.append('token', token);
 
     fetch(url,{
         method: 'POST',
-        body: 'formData',
-        mode: 'cors'
+        body: formData
     }).then(response => response.json())
     .then(data => {
-        if (data.ok) {
-            let elemento = document.getElementById("num_cart")
-            elemento.innerHTML = data.numero
+        if (data.hasOwnProperty('ok') && data.ok) {
+            let elemento = document.getElementById("num_cart");
+            let numero = parseInt(data.numero);
+            elemento.innerHTML = numero;
         }
-    })
-};
+    });
+}
