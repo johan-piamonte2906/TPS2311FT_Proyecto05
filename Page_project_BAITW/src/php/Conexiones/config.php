66f22bd77e97d3b7c56fbadfdf60ec1416cccaf5
johan-piamonte2906 <jhpiamonte16@misena.edu.co>
2023-09-06 16:17:34 -0500

    correcion de PHPMailer

--- Page_project_BAITW/src/php/Conexiones/config.php
+++ Page_project_BAITW/src/php/Conexiones/config.php
@@ -3,15 +3,15 @@
 define("SITE_URL", "http://localhost/TPS2311FT_Proyecto05/Page_project_BAITW/src/php/equipo-admin");
 define("CLIENT_ID", "AR5Eg-wA-i9iTLxYVYS7M9LGrO1vVKsC7VjgvqKDFpJS1QtFz_34aJvfILL5oG-sDR0yd4ZfwPxNn6wb");
 define("TOKEN_MP", "TEST-6509425968746122-060820-746f077cd7683e634b2c407faa478c1e-529804460");
-define("CURRENCY", "COP");
+define("CURRENCY", "CO");
 define("KEY_TOKEN", "JoHaN-2022*");
 define("MONEDA", "$");
 
 // correo
 
 define("MAIL_HOST","smtp.gmail.com");
-define("MAIL_USER","");
-define("MAIL_PASS","");
+define("MAIL_USER","projectbaitw.05@gmail.com");
+define("MAIL_PASS","cmxfazhcagklruxl");
 define("MAIL_PORT","465");
 
 
